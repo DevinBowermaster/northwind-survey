dd9f2950a6e89b5be4639900b889dbc8ec54bf44
Devin Bowermaster <devin@northwind.us>
2026-01-23 13:19:55 -0700

    Fix SurveyResponse import case

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
-import SurveyResponse from './Surveyresponse.jsx'
+import SurveyResponse from './SurveyResponse.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
